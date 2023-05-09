67c25d17bb0acf934e5b9fb18b3a60e2d0ef951b
Codeanderson17 <80644916+Codeanderson17@users.noreply.github.com>
2023-05-08 20:45:44 -0600

    logro
    frases

--- MENU LOVE/MENU/CORAZONES EXPOSIVOS/script.js
+++ MENU LOVE/MENU/CORAZONES EXPOSIVOS/script.js
@@ -15,7 +15,7 @@ function createHeart() {
     // Agrega un evento "click" al corazón
     heart.addEventListener("click", function() {
       // Reproduce una frase de amor al explotar el corazón
-      var phrases = ["Eres el amor de mi vida", "Te amo más que a nada en este mundo", "Mi corazón late por ti", "Eres mi razón de ser"];
+      var phrases = ["Eres el amor de mi vida", "Te amo más que a nada en este mundo", "Mi corazón late por ti", "Eres mi razón de ser","De nadie seré, sólo de ti. Hasta que mis huesos se vuelvan cenizas y mi corazón deje de latir","Andábamos sin buscarnos, pero sabiendo que andábamos para encontrarnos","Cuando mi voz calle con la muerte, mi corazón te seguirá amando","Miro las estrellas y ahí estas tu, cierro los ojos y ahí estas tú, siento con el corazón y ahí estas tú."," No sé hacia dónde vamos, solo sé que quiero ir contigo."," Ayer eras mi capricho. Hoy eres mi amor. Mañana serás mi vida.","Dime dónde y nos perdemos juntos.","Tu sonrisa es el paraíso.","Eres un pedazo del cielo en esta tierra."];
       var randomIndex = Math.floor(Math.random() * phrases.length);
       var audio = new Audio("amor.mp3");
       audio.play();
